c1645dda0ea8bf95ffa954cf081549dc5c092dee
7ABehrendt <7abehrendt@stpatrick.org>
2016-10-06 11:53:07 -0500

    Add files via upload
    My new octocat

---
+++ octocats.js
@@ -0,0 +1,22 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function octocat(n,a){
+  this.name = n;
+  this.arms = a;
+  var myoctocat = new octocat("Kuroka",2)
+  this.slap = function(a){
+    console.log(this.name)
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
